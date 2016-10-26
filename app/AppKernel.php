dfdbe6bc18d25a64934e1811d4285e7407588f4f
EtienneDh <etdhennin@gmail.com>
2016-10-26 16:04:31 +0200

    added static Bundle, configured routing.yml and Controller

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -17,6 +17,7 @@ class AppKernel extends Kernel
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new AppBundle\AppBundle(),
             new MMH\SiteBundle\MMHSiteBundle(),
+            new MMH\StaticBundle\MMHStaticBundle(),
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
