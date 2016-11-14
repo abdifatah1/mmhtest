cb3f16ca69aa45f1fb260bf2a5d336892be89c26
EtienneDh <etdhennin@gmail.com>
2016-11-14 12:05:59 +0100

    integrated progress bar for cards, wrote the Js script

--- src/MMH/SiteBundle/Resources/public/js/main.js
+++ src/MMH/SiteBundle/Resources/public/js/main.js
@@ -1,1 +1,9 @@
 
+// Set the width of the progress bar
+$(document).ready(function() {  
+  $('.progression').each(function(){
+    var width = $(this).children().text();
+    width >= 100 ? width = 100 : width = width;
+    $(this).css('width', width);
+  });
+});
