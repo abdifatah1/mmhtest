d1c7a9b7ad3364efdadd8a5bbbc4f42cfb5542b9
EtienneDh <etdhennin@gmail.com>
2016-11-21 16:37:50 +0100

    decouvrir les projets will now center on cards on load

--- src/MMH/SiteBundle/Resources/public/js/main.js
+++ src/MMH/SiteBundle/Resources/public/js/main.js
@@ -1,3 +1,5 @@
+// This file contains small scripts used for minor functionalities, for bigger component check labelled js files
+
 
 // Set the width of the cards progress bar
 $(document).ready(function() {
@@ -7,9 +9,20 @@ $(document).ready(function() {
     var width = $(this).children().text();
     // adjust margin if value > 100%
     if(width.length > 3) {
-      $(this).children().css('margin-left', '82px');  
+      $(this).children().css('margin-left', '82px');
     }
     // Apply
     $(this).css('width', width);
   });
+
+  // Center the view on cards on /découvrir-les-projets
+  var path = window.location.href;
+  var ele = $('.wrapper-nav');
+  var hashed = path.slice(-23);
+  if(hashed.search('decouvrir') != -1) {
+     $(window).scrollTop(ele.offset().top).scrollLeft(ele.offset().left);
+  }
 });
+
+
+/******************************************/
