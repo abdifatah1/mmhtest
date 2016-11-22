55beeb93f31c8a55bbd5ecd004a28a3bdb7d71b4
EtienneDh <etdhennin@gmail.com>
2016-11-22 10:33:20 +0100

    fixed search bar margin on smaller devices, wrote a js script that display the correct amount of cards according to resolution

--- src/MMH/SiteBundle/Resources/public/js/main.js
+++ src/MMH/SiteBundle/Resources/public/js/main.js
@@ -15,16 +15,39 @@ $(document).ready(function() {
     $(this).css('width', width);
   });
 
+  /*****************************************************/
+
   // Center the view on cards on /découvrir-les-projets
+  // get url path
   var path = window.location.href;
-  var ele = $('.wrapper-nav');
+  // get last part of url and check if url matches
   var hashed = path.slice(-23);
   if(hashed.search('decouvrir') != -1) {
+    // smooth scroll
     $('html, body').animate({
 		    scrollTop:$('.wrapper-nav').offset().top
     }, 'fast');
   }
 });
 
+/***********************************************************/
 
-/******************************************/
+// Adjust number of cards displayed according to resolution
+
+if (window.matchMedia("(max-width: 700px)").matches) {
+  $('.home-cards').each(function(index) {
+    // 3 cards displayed on smartphone
+    if(index >= 3) {
+      $(this).css('display', 'none');
+    }
+  });
+} else {
+  if (window.matchMedia("(max-width: 1010px)").matches) {
+    $('.home-cards').each(function(index) {
+      // 4 cards displayed on tablets and small screen
+      if(index >= 4) {
+        $(this).css('display', 'none');
+      }
+    });
+  }
+}
