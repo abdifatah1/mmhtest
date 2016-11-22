b56135d2d0e801af49c466c9904acddc8898ed38
EtienneDh <etdhennin@gmail.com>
2016-11-22 12:09:44 +0100

    worked on progressBar animation + auto scroll

--- src/MMH/SiteBundle/Resources/public/js/main.js
+++ src/MMH/SiteBundle/Resources/public/js/main.js
@@ -1,22 +1,6 @@
 // This file contains small scripts used for minor functionalities, for bigger component check labelled js files
 
-
-// Set the width of the cards progress bar
 $(document).ready(function() {
-  // Get every dom object which class ='progression'
-  $('.progression').each(function(){
-    // get the width of element from it's children
-    var width = $(this).children().text();
-    // adjust margin if value > 100%
-    if(width.length > 3) {
-      $(this).children().css('margin-left', '82px');
-    }
-    // Apply
-    $(this).css('width', width);
-  });
-
-  /*****************************************************/
-
   // Center the view on cards on /découvrir-les-projets
   // get url path
   var path = window.location.href;
@@ -26,10 +10,11 @@ $(document).ready(function() {
     // smooth scroll
     $('html, body').animate({
 		    scrollTop:$('.wrapper-nav').offset().top
-    }, 'fast');
+    }, 1000);
   }
 });
 
+/* End Document Ready */
 /***********************************************************/
 
 // Adjust number of cards displayed according to resolution
@@ -51,3 +36,23 @@ if (window.matchMedia("(max-width: 700px)").matches) {
     });
   }
 }
+
+/***********************************************************/
+
+// Trigger progressBar animation when user has scrolled down enough
+
+$(window).scroll(function() {
+  if ($(window).scrollTop() > $('.wrapper-nav').offset().top) {
+    // Get every dom object which class ='progression'
+    $('.progression').each(function(){
+    // get the width of element from it's children
+    var width = $(this).children().text();
+    // adjust margin if value > 100%
+    if(width.length > 3) {
+      $(this).children().css('margin-left', '82px');
+    }
+    // Apply
+    $(this).css('width', width);
+  });
+ }
+});
